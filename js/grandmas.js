429447cae4bca15328e96a61d90f8bc8b6a2bf0f
Vresod <40863375+Vresod@users.noreply.github.com>
2018-09-03 10:15:35 -0400

    more stuffffff

---
+++ js/grandmas.js
@@ -0,0 +1,20 @@
+var grandmas = 0;
+
+function buyGrandma(){
+    var grandmaCost = Math.floor(25 * Math.pow(1.1,grandmas));     //works out the cost of this grandma
+    if(cookies >= grandmaCost){                                   //checks that the player can afford the grandma
+        grandmas = grandmas + 1;                                   //increases number of grandmas
+        clicksoundlow()
+      cookies = cookies - grandmaCost;                          //removes the cookies spent
+        document.getElementById('grandmas').innerHTML = grandmas;  //updates the number of grandmas for the user
+        document.getElementById('cookies').innerHTML = cookies;  //updates the number of cookies for the user
+    };
+    var nextCost = Math.floor(25 * Math.pow(1.1,grandmas));       //works out the cost of the next grandma
+    document.getElementById('grandmaCost').innerHTML = nextCost;  //updates the grandma cost for the user
+};
+
+window.setInterval(function (){
+
+  cookieClick(grandmas * 2);
+
+}, 1000);
