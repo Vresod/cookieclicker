e5f34c0eb9c6236563ced9bcfb149e7255524f7a
Vresod <40863375+Vresod@users.noreply.github.com>
2018-11-10 18:31:40 -0500

    literally broke everything WIP

--- js/cookiecowboys.js
+++ js/cookiecowboys.js
@@ -1,20 +1,18 @@
-var cookiecowboys = 0;
-
 function buyCookieCowboy() {
-	var cookiecowboyCost = Math.floor(50 * Math.pow(1.1, cookiecowboys)); //works out the cost of this cookiecowboy
+	costs.cookiecowboyCost = Math.floor(50 * Math.pow(1.1, upgrades.cookiecowboys)); //works out the cost of this cookiecowboy
 	if (cookies >= cookiecowboyCost) { //checks that the player can afford the cookiecowboy
-		cookiecowboys = cookiecowboys + 1; //increases number of cookiecowboys
+		upgrades.cookiecowboys = upgrades.cookiecowboys + 1; //increases number of cookiecowboys
 		clicksoundlow()
-		cookies = cookies - cookiecowboyCost; //removes the cookies spent
-		document.getElementById('cookiecowboys').innerHTML = cookiecowboys; //updates the number of cookiecowboys for the user
-		document.getElementById('cookies').innerHTML = cookies; //updates the number of cookies for the user
+		stats.cookies = stats.cookies - costs.cookiecowboyCost; //removes the cookies spent
+		document.getElementById('cookiecowboys').innerHTML = upgrades.cookiecowboys; //updates the number of cookiecowboys for the user
+		document.getElementById('cookies').innerHTML = stats.cookies; //updates the number of cookies for the user
 	};
-	var nextCost = Math.floor(50 * Math.pow(1.1, cookiecowboys)); //works out the cost of the next cookiecowboy
+	var nextCost = Math.floor(50 * Math.pow(1.1, upgrades.cookiecowboys)); //works out the cost of the next cookiecowboy
 	document.getElementById('cookiecowboyCost').innerHTML = nextCost; //updates the cookiecowboy cost for the user
 };
 
 window.setInterval(function () {
 
-	cookieClick(cookiecowboys * 5);
+	cookieClick(upgrades.cookiecowboys * 5);
 
 }, 1000);
--- js/game.js
+++ js/game.js
@@ -1,6 +1,25 @@
-var cookies = 0;
-
-var cpc = 1;
+ var stats = {
+	"cookies": 0,
+	"cps": 0,
+}
+var upgrades = {
+	"cursors": 0,
+	"grandmas": 0,
+	"cookiecowboys": 0,
+	"cookiepropagandas": 0;
+	"cookiejesuses": 0,
+}
+var costs = {
+	"cursorCost": 0,
+	"grandmaCost": 0,
+	"cookiecowboyCost": 0,
+	"cookiepropagandas": 0,
+	"cookiejesusCost": 0,
+}
+var penalties = {
+	"autoBuy": false,
+	"fakeCookies": false,
+}
 
 function clicksound() {
 	document.getElementById('click').play();
@@ -11,17 +30,29 @@ function clicksoundlow() {
 };
 
 function cookieClick(number) {
-	cookies = cookies + number;
-	document.getElementById("cookies").innerHTML = cookies;
-	document.title = cookies + ' Cookies - Cookie Clicker'
+	stats.cookies = stats.cookies + number;
+	document.getElementById("cookies").innerHTML = stats.cookies;
+	document.title = stats.cookies + ' Cookies - Cookie Clicker'
 };
 
 window.setInterval(function () {
-	var cps = cursors + (grandmas * 2) + (cookiecowboys * 5) + (cookiepropagandas * 25) + (cookiejesuses * 50);
-	document.getElementById('cps').innerHTML = cps;
+	stats.cps = cursors + (grandmas * 2) + (cookiecowboys * 5) + (cookiepropagandas * 25) + (cookiejesuses * 50);
+	document.getElementById('cps').innerHTML = stats.cps;
 }, 10);
 
-function cheat(number) {
-	cookieClick(number);
+function autoBuy() {
+	window.setInterval(function () {
+		buyCookieCowboy();
+		buyCookieJesus();
+		buyCursor();
+		buyGrandma();
+		buyCookiePropaganda();
+		penalties.autoBuy = true;
+	}, 10)
+}
 
+function fakeCookies() {
+	window.setinterval(function () {
+		penalties.fakeCookies = true;
+	}, 10)
 }
